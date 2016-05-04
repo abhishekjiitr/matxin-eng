b507380b95b11da4b2e4e6ad4a784dbfa9a501e9
Francis Tyers <francis.tyers@uit.no>
2016-05-04 23:17:50 +0200

    some stanford stuff

---
+++ stanford/get-dependencies.sh
@@ -0,0 +1,5 @@
+echo "Caution: 259M"
+wget http://nlp.stanford.edu/software/stanford-corenlp-2012-04-09.tgz
+tar -xzf stanford-corenlp-2012-04-09.tgz
+mv stanford-corenlp-2012-04-09/stanford-corenlp-2012-04-09.jar .
+rm -r stanford-corenlp-2012-04-09
